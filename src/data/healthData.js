ae7d640c99c74b29e211410d6a34c6d87f7684a1
akshaybawane008 <akshaybawane008gmail.com>
2025-05-28 14:59:45 +0530

    All Done

--- src/data/healthData.js
+++ src/data/healthData.js
@@ -1,4 +1,6 @@
-
+import Bons from "../assets/Bons.png"
+import Lungs from "../assets/Lungs.png"
+import Teeth from "../assets/Teeth.png"
 
 export const healthIndicators = [
   { label: 'Healthy Heart', colorClass: 'green', top: '20%', left: '30%' },
@@ -12,21 +14,21 @@ export const healthCards = [
     title: 'Lungs',
     status: 'Issue',
     date: 'Date: 24 May 2025',
-    image: '../../src/assets/Lungs.png',
+    image: Lungs,
     color: "#D3212C"
   },
   {
     title: 'Teeth',
     status: 'OK',
     date: 'Date: 23 May 2025',
-    image: '../../src/assets/Teeth.png',
+    image: Teeth,
     color: "#069C56"
   },
   {
     title: 'Bone',
     status: 'OK',
     date: 'Date: 21 May 2025',
-    image: '../../src/assets/Bons.png',
+    image: Bons,
     color: "#FF681E"
   }
 ]
